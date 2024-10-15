e73b829fcabbfec104c42c42eccf11e6c3a2c0d4
shiyingjie <shiyingjie@sct1039.com>
2024-10-15 09:19:53 +0800

    chore: setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	test: {
+		globals: true,
+		environment: 'jsdom',
+	},
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': './src',
+		},
+	},
 })
