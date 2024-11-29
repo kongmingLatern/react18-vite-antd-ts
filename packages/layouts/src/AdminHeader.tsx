da2b24445fe0531f19230c6308fe6636cd6f20e1
风之兮原 <1577310596@qq.com>
2024-11-29 15:29:36 +0800

    feat: add header breadcrumb

--- packages/layouts/src/AdminHeader.tsx
+++ packages/layouts/src/AdminHeader.tsx
@@ -1,5 +1,14 @@
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons'
 import { Space } from 'antd'
+import { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { menuMap } from './AdminBreadcrumb'
+
+interface BreadcrumbItem {
+  path: string
+  label: string
+  icon?: React.ReactNode
+}
 
 interface AdminHeaderProps {
   collapsed: boolean
@@ -8,11 +17,59 @@ interface AdminHeaderProps {
 
 export default function AdminHeader(props: AdminHeaderProps) {
   const { collapsed, onCollapse } = props
+  const location = useLocation()
+  const navigate = useNavigate()
+  const [breadcrumbs, setBreadcrumbs] = useState<BreadcrumbItem[]>([])
+
+  useEffect(() => {
+    const paths = location.pathname.split('/').filter(Boolean)
+    const currentPath = `/${paths.join('/')}`
+    const currentMenu = menuMap[currentPath as keyof typeof menuMap]
+
+    if (currentMenu) {
+      const newBreadcrumbs: BreadcrumbItem[] = []
+
+      // Build up breadcrumbs starting from first level
+      let currentBuildPath = ''
+      for (let i = 0; i < paths.length; i++) {
+        currentBuildPath += `/${paths[i]}`
+        const menuItem = menuMap[currentBuildPath as keyof typeof menuMap]
+        if (menuItem) {
+          newBreadcrumbs.push({
+            path: currentBuildPath,
+            label: menuItem.label,
+            icon: menuItem.icon,
+          })
+        }
+      }
+
+      setBreadcrumbs(newBreadcrumbs)
+    }
+  }, [location.pathname])
+
   return (
     <Space>
-      <span className="cursor-pointer" onClick={() => onCollapse(!collapsed)}>
-        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-      </span>
+      <Space size={30}>
+        <span className="cursor-pointer text-14px" onClick={() => onCollapse(!collapsed)}>
+          {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        </span>
+        <span className="text-14px">
+          {breadcrumbs.map((item, index) => (
+            <span
+              key={item.path}
+              style={{
+                color: item.path === location.pathname ? 'rgb(51, 54, 57)' : 'rgb(118, 124, 130)',
+                cursor: 'pointer',
+              }}
+              onClick={() => navigate(item.path)}
+            >
+              {item.icon}
+              <span className="mx-1">{item.label}</span>
+              {index < breadcrumbs.length - 1 && <span className="mx-1">/</span>}
+            </span>
+          ))}
+        </span>
+      </Space>
     </Space>
   )
 }
